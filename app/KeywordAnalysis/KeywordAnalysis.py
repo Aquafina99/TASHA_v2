1cc98fecabbe659d0befa75b518693cb6e85b6da
rjynn <rdejesus@ualberta.ca>
2018-05-08 15:20:30 -0600

    adding next function

--- app/KeywordAnalysis/KeywordAnalysis.py
+++ app/KeywordAnalysis/KeywordAnalysis.py
@@ -8,6 +8,8 @@ from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize, pos_tag
 from six import iteritems
 from datetime import date
+from gensim import corpora
+
 
 
 class KeywordAnalysis:
@@ -26,14 +28,11 @@ class KeywordAnalysis:
         The function assumes the text is at the cell with index 4.
         
         Args: documents (json) json object that contains an array of collection of documents
-    
         Returns: bool: List of documents represented as a list of tokens
         """
         docs = []
-    
         # create English stop words list
         stopwords = get_stop_words('en')
-        
         # create wordnet lemmatizer
         lemmatizer = WordNetLemmatizer()
         
@@ -63,3 +62,46 @@ class KeywordAnalysis:
                 print("  Processing document " + str(doc_counter))
         return docs
 
+    def createDictionary(self, docs, output = 'dictionary.dict'):
+        """Creates a dictionary from a list of tokenized documents.  
+        This procedure removes extremely popular and infrequent words.
+        Args: docs (lst): List of tokenized documents
+        Returns: bool: The dictionary
+        """
+        # turn our tokenized documents into a id <-> term dictionary
+        dictionary = corpora.Dictionary(docs)
+        #dictionary.save('dictionary.dct')
+        
+        # If a dictionary already exists, replace the previous two line by the following line
+        # dictionary = corpora.Dictionary.load('dictionary.dct')
+        
+        freq = [doc_freq for word_id, doc_freq in iteritems(dictionary.dfs)]
+        #self.plotHistogram(freq, x_label = 'Words', y_label = 'Frequency', title = 'Dictionary Histogram - Unfiltered', 
+        #              bins = 100, output = 'dict-unfiltered.png')
+        
+        # remove very common words (appear in more than 25% of docs) and words with very low frequency (appear in less than 5 docs)
+        dictionary.filter_extremes(no_above = NO_ABOVE, no_below = NO_BELOW)
+        freq = [doc_freq for word_id, doc_freq in iteritems(dictionary.dfs)]
+        #self.plotHistogram(freq, x_label = 'Words', y_label = 'Frequency', title = 'Dictionary Histogram - Filtered', 
+        #              bins = 100, output = 'dict-filtered.png')
+        
+        return dictionary
+    
+    def plotHistogram(data, x_label = "X", y_label = "Frequency", title = "", bins = 20, output = "output.png"):
+        """Plots a histogram and stores it as a PNG file.  
+    
+        Args:
+            data (array): Array of data
+            x_label (str): Label of the X axis
+            y_label (str): Label of the Y axis
+            title (str): Title of the histogram
+            bins (int): Bins of the histogram
+            output (str): File name for the image file to be stored
+        """
+        plt.clf()
+        plt.hist(data, bins = bins)
+        plt.title(title)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        
+        plt.savefig(output)    
